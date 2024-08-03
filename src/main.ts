9133a5edfb7dce228b656bac4a6d4b237cf8fdc5
Yury Odintsov <yury.odinson@gmail.com>
2024-08-03 14:15:59 +0300

    feat: signIn form

--- src/main.ts
+++ src/main.ts
@@ -35,7 +35,7 @@ function navigate(page: string) {
     container.innerHTML = templatingFunction(context);
 }
 
-document.addEventListener("DOMContentLoaded", () => navigate("login"));
+document.addEventListener("DOMContentLoaded", () => navigate("signIn"));
 
 document.addEventListener('click', e => {
     //@ts-ignore
